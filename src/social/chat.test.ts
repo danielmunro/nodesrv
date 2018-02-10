3a6faa964a0bb31074be16a3634adf894fdaf63f
Dan Munro <dan@danmunro.com>
2018-02-10 17:51:10 -0600

    tests

---
+++ src/social/chat.test.ts
@@ -0,0 +1,21 @@
+import { Player } from "./../player/player"
+import { Channel } from "./channel"
+import { broadcastMessage, readMessages } from "./chat"
+
+describe("chat", () => {
+  it("should be able to broadcast and receive messages", () => {
+    const player = new Player()
+    const message = "hello world"
+    broadcastMessage(
+      player,
+      Channel.Gossip,
+      message,
+    )
+    const messages = readMessages()
+    expect(messages.length).toBe(1)
+    expect(messages[0].getSender()).toBe(player)
+    expect(messages[0].getChannel()).toBe(Channel.Gossip)
+    expect(messages[0].getMessage()).toBe(message)
+    expect(readMessages().length).toBe(0)
+  })
+})
