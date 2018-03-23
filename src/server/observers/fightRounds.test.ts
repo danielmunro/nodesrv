89c86e76f03cb79aafa758eca8d00540f25ac45d
Dan Munro <dan@danmunro.com>
2018-03-23 07:57:51 -0500

    less magic values

--- src/server/observers/fightRounds.test.ts
+++ src/server/observers/fightRounds.test.ts
@@ -6,13 +6,15 @@ describe("fight rounds", () => {
   it("should generate accurate attack messages", () => {
     const mob1 = getTestMob("mob1")
     const mob2 = getTestMob("mob2")
-    mob2.vitals.hp -= 1
-    const attack1 = new Attack(mob1, mob2, AttackResult.Hit, 1)
+    const damageAmount = 1
+    mob2.vitals.hp -= damageAmount
+    const attack1 = new Attack(mob1, mob2, AttackResult.Hit, damageAmount)
     expect(attackMessage(attack1, mob1)).toContain("You hit mob2")
     expect(attackMessage(attack1, mob2)).toContain("mob1 hits you")
 
-    mob1.vitals.hp -= 100
-    const attack2 = new Attack(mob2, mob1, AttackResult.Hit, 100)
+    const newDamageAmount = 100
+    mob1.vitals.hp -= newDamageAmount
+    const attack2 = new Attack(mob2, mob1, AttackResult.Hit, newDamageAmount)
     expect(attackMessage(attack2, mob1)).toContain("You have DIED")
     expect(attackMessage(attack2, mob2)).toContain("mob1 has DIED")
   })
