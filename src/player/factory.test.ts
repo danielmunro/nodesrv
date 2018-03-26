f591130cfa6851b2a4275bc581def14ae1ad126b
Dan Munro <dan@danmunro.com>
2018-03-26 06:53:45 -0500

    test for player factory

---
+++ src/player/factory.test.ts
@@ -0,0 +1,13 @@
+import { getTestMob } from "../test/mob"
+import { newPlayer } from "./factory"
+
+describe("player factory", () => {
+  it("should set the name and session mob", () => {
+    const name = "a test player"
+    const sessionMob = getTestMob()
+    const player = newPlayer(name, sessionMob)
+    expect(player.name).toBe(name)
+    expect(player.sessionMob).toBe(sessionMob)
+    expect(player.mobs.indexOf(sessionMob)).toBeGreaterThanOrEqual(0)
+  })
+})
