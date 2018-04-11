4fcfab726fc8a37ba99a70feb316993fd807170c
Dan Munro <dan@danmunro.com>
2018-04-11 07:22:40 -0500

    initial docker integration for postgres

---
+++ bin/sync-orm-config.ts
@@ -0,0 +1,12 @@
+import * as fs from "fs"
+import { Connection } from "typeorm"
+import { newInn, newTrail } from "../src/area/factory"
+import { getConnection } from "../src/db/connection"
+import { Direction } from "../src/room/constants"
+import { getFreeDirection } from "../src/room/direction"
+import { newRoom } from "../src/room/factory"
+
+const ormConfig = JSON.parse(fs.readFileSync("ormconfig.json").toString())
+ormConfig.synchronize = true
+ormConfig.logging = true
+getConnection(ormConfig)
