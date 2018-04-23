676aadf7fd8ac5e27ca05fe85ddfe58a445aeb2d
Dan Munro <dan@danmunro.com>
2018-04-23 06:38:12 -0500

    concurrent modification fix

--- bin/create-fixtures.ts
+++ bin/create-fixtures.ts
@@ -1,8 +1,12 @@
 import { newWorld } from "../src/area/factory"
+import { getConnection } from "../src/db/connection"
 import { newRoom } from "../src/room/factory"
 
-const rootRoom = newRoom(
-  "A clearing in the woods",
-  "A small patch of land has been cleared of trees. On it sits a modest inn, tending to weary travellers.")
+getConnection()
+  .then(() => {
+    const rootRoom = newRoom(
+      "A clearing in the woods",
+      "A small patch of land has been cleared of trees. On it sits a modest inn, tending to weary travellers.")
 
-newWorld(rootRoom)
+    newWorld(rootRoom)
+  })
