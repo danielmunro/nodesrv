de74880a4bdd1563d80992787138d3c92e53c1c3
Dan Munro <dan@danmunro.com>
2018-04-30 07:05:57 -0500

    refactor bash test

--- src/skill/actions/bash.test.ts
+++ src/skill/actions/bash.test.ts
@@ -1,62 +1,72 @@
 import { RequestType } from "../../handler/constants"
-import { addFight, Fight } from "../../mob/fight/fight"
+import { addFight, Fight, reset } from "../../mob/fight/fight"
 import { Player } from "../../player/model/player"
 import { createRequestArgs, Request } from "../../server/request/request"
-import { Skill } from "../../skill/model/skill"
 import { SkillType } from "../../skill/skillType"
 import { getTestMob } from "../../test/mob"
 import { getTestPlayer } from "../../test/player"
 import { newSkill } from "../factory"
 import bash, { MESSAGE_FAIL, MESSAGE_NO_SKILL, MESSAGE_NO_TARGET } from "./bash"
 
+const RETRY_COUNT = 10
+
 function createBashRequest(player: Player): Request {
   return new Request(player, RequestType.Bash, createRequestArgs("bash"))
 }
 
+function bashRepeater(player: Player) {
+  return () => bash(createBashRequest(player))
+}
+
+function times(count: number, callback) {
+  return Array.from(Array(10).keys()).map((n) => callback())
+}
+
+function addNewTestFight(player: Player): void {
+  addFight(new Fight(player.sessionMob, getTestMob()))
+}
+
+beforeEach(() => reset())
+
 describe("bash", () => {
   it("should not work if a mob is not in combat", async () => {
     expect.assertions(1)
-
     await bash(createBashRequest(getTestPlayer()))
       .then((result) => expect(result).toEqual({ message: MESSAGE_NO_TARGET }))
   })
 
   it("should not work if a mob does not have the skill", async () => {
+    // setup
     const player = getTestPlayer()
-    const target = getTestMob()
-    const fight = new Fight(player.sessionMob, target)
-    addFight(fight)
+    addNewTestFight(player)
     expect.assertions(1)
 
+    // expect
     await bash(createBashRequest(player))
       .then((result) => expect(result).toEqual({ message: MESSAGE_NO_SKILL }))
   })
 
   it("should be able to trigger a failed bash", async () => {
+    // setup
     const player = getTestPlayer()
     player.sessionMob.skills.push(newSkill(SkillType.Bash))
-    const target = getTestMob()
-    const fight = new Fight(player.sessionMob, target)
-    addFight(fight)
+    addNewTestFight(player)
     expect.assertions(1)
 
-    const bashRepeater = () => bash(createBashRequest(player))
-
-    await Promise.all(Array.from(Array(10).keys()).map((n) => bashRepeater()))
-      .then((results) => expect(results.find((result) => result.message === MESSAGE_FAIL)).toBeTruthy())
+    // expect
+    await Promise.all(times(RETRY_COUNT, bashRepeater(player)))
+      .then((results) => expect(results.some((result) => result.message === MESSAGE_FAIL)).toBeTruthy())
   })
 
   it("should be able to trigger a successful bash", async () => {
+    // setup
     const player = getTestPlayer()
     player.sessionMob.skills.push(newSkill(SkillType.Bash, 100))
-    const target = getTestMob()
-    const fight = new Fight(player.sessionMob, target)
-    addFight(fight)
+    addNewTestFight(player)
     expect.assertions(1)
 
-    const bashRepeater = () => bash(createBashRequest(player))
-
-    await Promise.all(Array.from(Array(10).keys()).map((n) => bashRepeater()))
-      .then((results) => expect(results.find((result) => result.message.includes("You slam into"))).toBeTruthy())
+    // expect:
+    await Promise.all(times(RETRY_COUNT, bashRepeater(player)))
+      .then((results) => expect(results.some((result) => result.message.includes("You slam into"))).toBeTruthy())
   })
 })
