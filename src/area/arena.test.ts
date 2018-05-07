35feda7b2448bb0d4ecc1d81b406ff1a68eec000
Dan Munro <dan@danmunro.com>
2018-05-07 06:44:20 -0500

    remove some boilerplate, tweak assertions

--- src/area/arena.test.ts
+++ src/area/arena.test.ts
@@ -3,32 +3,46 @@ import { Direction } from "../room/constants"
 import { Room } from "../room/model/room"
 import { Arena } from "./arena"
 
+const width = 5
+const height = 6
+let arena
+
 describe("arena", () => {
+  beforeEach(() => arena = new Arena(new Room(), width, height, newCritter))
+
   it("should build a matrix of requested size", () => {
-    const width = 5
-    const height = 6
-    const arena = new Arena(new Room(), width, height, newCritter)
     expect(arena.matrix.length).toBe(height)
     arena.matrix.forEach((row) => expect(row.length).toBe(width))
   })
 
   it("should have directional exits connecting each room", () => {
-    const width = 5
-    const height = 6
-    const arena = new Arena(new Room(), width, height, newCritter)
-
     arena.matrix.forEach((row, y) => row.forEach((room: Room, x) => {
-      // below conditionals cover the random connection of the root room
-      if (x > 0) {
-        expect(room.isDirectionFree(Direction.North)).toBe(y === 0)
+      // edges of arena
+      if (x === 0) {
+        expect(room.isDirectionFree(Direction.East)).toBe(false)
       }
-      if (y > 0) {
-        expect(room.isDirectionFree(Direction.West)).toBe(x === 0)
-      }
-      expect(room.isDirectionFree(Direction.East)).toBe(x === width - 1)
-      expect(room.isDirectionFree(Direction.South)).toBe(y === height - 1)
 
-      // arenas do not extend up or down
+      if (y === 0) {
+        expect(room.isDirectionFree(Direction.South)).toBe(false)
+      }
+
+      if (x === row.length - 1) {
+        expect(room.isDirectionFree(Direction.West)).toBe(false)
+      }
+
+      if (y === arena.matrix.length - 1) {
+        expect(room.isDirectionFree(Direction.North)).toBe(false)
+      }
+
+      // non-edges of marina
+      if (x > 0 && x < row.length - 1 && y > 0 && y < arena.matrix.length - 1) {
+        expect(room.isDirectionFree(Direction.North)).toBe(false)
+        expect(room.isDirectionFree(Direction.East)).toBe(false)
+        expect(room.isDirectionFree(Direction.South)).toBe(false)
+        expect(room.isDirectionFree(Direction.West)).toBe(false)
+      }
+
+      // arenas are flat
       expect(room.isDirectionFree(Direction.Up)).toBe(true)
       expect(room.isDirectionFree(Direction.Down)).toBe(true)
     }))
