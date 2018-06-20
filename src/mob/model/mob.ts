34706bef5d0b19dd769cd8b86e8fbdc867cb4828
Dan Munro <dan@danmunro.com>
2018-06-20 04:48:30 -0500

    formatting

--- src/mob/model/mob.ts
+++ src/mob/model/mob.ts
@@ -17,86 +17,86 @@ import { SpecializationType } from "../specialization/specializationType"
 
 @Entity()
 export class Mob {
-    @PrimaryGeneratedColumn()
-    public id: number
+  @PrimaryGeneratedColumn()
+  public id: number
 
-    @Column("text")
-    @Generated("uuid")
-    public uuid: string = v4()
+  @Column("text")
+  @Generated("uuid")
+  public uuid: string = v4()
 
-    @Column("text")
-    public name: string
+  @Column("text")
+  public name: string
 
-    @Column("text", { nullable: true })
-    public description: string
+  @Column("text", { nullable: true })
+  public description: string
 
-    @Column("text")
-    public race: Race
+  @Column("text")
+  public race: Race
 
-    @Column("text", { nullable: true })
-    public specialization: SpecializationType
+  @Column("text", { nullable: true })
+  public specialization: SpecializationType
 
-    @Column("integer")
-    public level: number = 1
+  @Column("integer")
+  public level: number = 1
 
-    @Column("boolean")
-    public wanders: boolean = false
+  @Column("boolean")
+  public wanders: boolean = false
 
-    @Column("boolean")
-    public isPlayer: boolean = false
+  @Column("boolean")
+  public isPlayer: boolean = false
 
-    @OneToMany((type) => Affect, (affect) => affect.mob, { cascadeInsert: true, eager: true })
-    public affects: Affect[] = []
+  @OneToMany((type) => Affect, (affect) => affect.mob, { cascadeInsert: true, eager: true })
+  public affects: Affect[] = []
 
-    @OneToOne((type) => Vitals, { cascadeInsert: true, eager: true })
-    @JoinColumn()
-    public vitals: Vitals = new Vitals()
+  @OneToOne((type) => Vitals, { cascadeInsert: true, eager: true })
+  @JoinColumn()
+  public vitals: Vitals = new Vitals()
 
-    @OneToMany((type) => Attributes, (attributes) => attributes.mob, { cascadeInsert: true, eager: true })
-    public attributes: Attributes[] = []
+  @OneToMany((type) => Attributes, (attributes) => attributes.mob, { cascadeInsert: true, eager: true })
+  public attributes: Attributes[] = []
 
-    @ManyToOne((type) => Room, (room) => room.mobs, { eager: true })
-    public room: Room
+  @ManyToOne((type) => Room, (room) => room.mobs, { eager: true })
+  public room: Room
 
-    @ManyToOne((type) => Room, { eager: true })
-    @JoinColumn()
-    public startRoom: Room
+  @ManyToOne((type) => Room, { eager: true })
+  @JoinColumn()
+  public startRoom: Room
 
-    @ManyToOne((type) => Player, (player) => player.mobs, { eager: true })
-    public player: Player
+  @ManyToOne((type) => Player, (player) => player.mobs, { eager: true })
+  public player: Player
 
-    @OneToOne((type) => Inventory, { cascadeInsert: true, eager: true })
-    @JoinColumn()
-    public inventory = new Inventory()
+  @OneToOne((type) => Inventory, { cascadeInsert: true, eager: true })
+  @JoinColumn()
+  public inventory = new Inventory()
 
-    @OneToOne((type) => Equipped, { cascadeInsert: true, eager: true })
-    @JoinColumn()
-    public equipped = new Equipped()
+  @OneToOne((type) => Equipped, { cascadeInsert: true, eager: true })
+  @JoinColumn()
+  public equipped = new Equipped()
 
-    @OneToMany((type) => Skill, (skill) => skill.mob, { cascadeInsert: true, cascadeUpdate: true })
-    public skills: Skill[] = []
+  @OneToMany((type) => Skill, (skill) => skill.mob, { cascadeInsert: true, cascadeUpdate: true })
+  public skills: Skill[] = []
 
-    @OneToMany((type) => Spell, (spell) => spell.mob, { cascadeInsert: true, cascadeUpdate: true })
-    public spells: Spell[] = []
+  @OneToMany((type) => Spell, (spell) => spell.mob, { cascadeInsert: true, cascadeUpdate: true })
+  public spells: Spell[] = []
 
-    public getCombinedAttributes(): Attributes {
-      let attributes = newAttributes(newVitals(0, 0, 0), newStats(0, 0, 0, 0, 0, 0), newHitroll(0, 0))
-      this.attributes.map((a) => attributes = attributes.combine(a))
-      this.equipped.inventory.items.map((i) => attributes = attributes.combine(i.attributes))
-      modifiers.map((modifier) => attributes = modifier(this.race, attributes))
+  public getCombinedAttributes(): Attributes {
+    let attributes = newAttributes(newVitals(0, 0, 0), newStats(0, 0, 0, 0, 0, 0), newHitroll(0, 0))
+    this.attributes.map((a) => attributes = attributes.combine(a))
+    this.equipped.inventory.items.map((i) => attributes = attributes.combine(i.attributes))
+    modifiers.map((modifier) => attributes = modifier(this.race, attributes))
 
-      return attributes
+    return attributes
+  }
+
+  public addAffect(affect: Affect) {
+    const current = this.getAffect(affect.affectType)
+    if (!current) {
+      this.affects.push(affect)
+      affect.mob = this
     }
+  }
 
-    public addAffect(affect: Affect) {
-      const current = this.getAffect(affect.affectType)
-      if (!current) {
-        this.affects.push(affect)
-        affect.mob = this
-      }
-    }
-
-    public getAffect(affectType: AffectType) {
-      return this.affects.find((a) => a.affectType === affectType)
-    }
+  public getAffect(affectType: AffectType) {
+    return this.affects.find((a) => a.affectType === affectType)
+  }
 }
