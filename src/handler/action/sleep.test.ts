fa45f2a11705c932f53d806173ff87011cabb492
Dan Munro <dan@danmunro.com>
2018-06-30 22:21:28 -0500

    lint

--- src/handler/action/sleep.test.ts
+++ src/handler/action/sleep.test.ts
@@ -1,8 +1,8 @@
+import { Disposition } from "../../mob/disposition"
 import { RequestType } from "../../request/requestType"
 import { ResponseStatus } from "../../request/responseStatus"
 import TestBuilder from "../../test/testBuilder"
 import sleep, { MESSAGE_SLEEP_SUCCESS } from "./sleep"
-import { Disposition } from "../../mob/disposition"
 
 describe("sleep action handler", () => {
   it("should change the mob's disposition to sleeping", async () => {
