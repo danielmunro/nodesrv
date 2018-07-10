872533613dd82b6e6fd1357173f159bbf8e8be13
Dan Munro <dan@danmunro.com>
2018-07-10 09:23:11 -0500

    more test cases

--- src/action/actions/train.test.ts
+++ src/action/actions/train.test.ts
@@ -4,7 +4,7 @@ import { ResponseStatus } from "../../request/responseStatus"
 import TestBuilder from "../../test/testBuilder"
 import Check from "../check"
 import CheckedRequest from "../checkedRequest"
-import train from "./train"
+import train, { MAX_TRAINABLE_STATS, MESSAGE_FAIL_CANNOT_TRAIN } from "./train"
 
 describe("train action", () => {
   it("should fail if a requested train is not understood", async () => {
@@ -150,5 +150,56 @@ describe("train action", () => {
     expect(response3.status).toBe(ResponseStatus.Success)
     expect(player.sessionMob.trains).toBe(0)
     expect(player.sessionMob.trainedAttributes.vitals.mv).toBe(initialMv + 10)
+  })
+
+  it("should not exceed stat max training amounts", async () => {
+    // given
+    const testBuilder = new TestBuilder()
+    const player = testBuilder.withPlayer().player
+    const trainer = testBuilder.withTrainer().mob
+    player.sessionMob.trains = 10
+    player.sessionMob.trainedAttributes.stats.str = MAX_TRAINABLE_STATS
+    player.sessionMob.trainedAttributes.stats.int = MAX_TRAINABLE_STATS
+    player.sessionMob.trainedAttributes.stats.wis = MAX_TRAINABLE_STATS
+    player.sessionMob.trainedAttributes.stats.dex = MAX_TRAINABLE_STATS
+    player.sessionMob.trainedAttributes.stats.con = MAX_TRAINABLE_STATS
+    player.sessionMob.trainedAttributes.stats.sta = MAX_TRAINABLE_STATS
+
+    // when
+    const response1 = await train(new CheckedRequest(
+      new Request(player, RequestType.Train, "train str"),
+      await Check.ok(trainer)))
+    expect(response1.status).toBe(ResponseStatus.ActionFailed)
+    expect(response1.message).toBe(MESSAGE_FAIL_CANNOT_TRAIN)
+
+    const response2 = await train(new CheckedRequest(
+      new Request(player, RequestType.Train, "train int"),
+      await Check.ok(trainer)))
+    expect(response2.status).toBe(ResponseStatus.ActionFailed)
+    expect(response2.message).toBe(MESSAGE_FAIL_CANNOT_TRAIN)
+
+    const response3 = await train(new CheckedRequest(
+      new Request(player, RequestType.Train, "train wis"),
+      await Check.ok(trainer)))
+    expect(response3.status).toBe(ResponseStatus.ActionFailed)
+    expect(response3.message).toBe(MESSAGE_FAIL_CANNOT_TRAIN)
+
+    const response4 = await train(new CheckedRequest(
+      new Request(player, RequestType.Train, "train dex"),
+      await Check.ok(trainer)))
+    expect(response4.status).toBe(ResponseStatus.ActionFailed)
+    expect(response4.message).toBe(MESSAGE_FAIL_CANNOT_TRAIN)
+
+    const response5 = await train(new CheckedRequest(
+      new Request(player, RequestType.Train, "train con"),
+      await Check.ok(trainer)))
+    expect(response5.status).toBe(ResponseStatus.ActionFailed)
+    expect(response5.message).toBe(MESSAGE_FAIL_CANNOT_TRAIN)
+
+    const response6 = await train(new CheckedRequest(
+      new Request(player, RequestType.Train, "train sta"),
+      await Check.ok(trainer)))
+    expect(response6.status).toBe(ResponseStatus.ActionFailed)
+    expect(response6.message).toBe(MESSAGE_FAIL_CANNOT_TRAIN)
   })
 })
