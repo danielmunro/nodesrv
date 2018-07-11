27ffdf975e4538e6039628d7d520248f9e34d801
Dan Munro <dan@danmunro.com>
2018-07-10 22:09:15 -0500

    use the last room to connect

--- src/area/factory.ts
+++ src/area/factory.ts
@@ -12,12 +12,12 @@ export async function newWorld(rootRoom: Room): Promise<Set<Room>> {
   const ARENA_1_HEIGHT = 5
   const inn = await newInn(rootRoom)
   const trail1 = await newTrail(rootRoom, getFreeDirection(rootRoom), TRAIL_1_ROOMS_TO_BUILD)
-  const trail1Ending = trail1[TRAIL_2_ROOMS_TO_BUILD - 1]
+  const trail1Ending = trail1[trail1.length - 1]
   const trail2 = await newTrail(
     trail1Ending,
     getFreeDirection(trail1Ending),
     TRAIL_2_ROOMS_TO_BUILD)
-  const clearing = await newClearing(trail2[2], ARENA_1_WIDTH, ARENA_1_HEIGHT)
+  const clearing = await newClearing(trail2[trail2.length - 1], ARENA_1_WIDTH, ARENA_1_HEIGHT)
 
   return new Set([
     ...inn,
