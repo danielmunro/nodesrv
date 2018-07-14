424bb5003d6371df319c0d6381ffb6fd7bec9c9e
Dan Munro <dan@danmunro.com>
2018-07-14 07:44:00 -0500

    movement tests and remove errant comment

--- src/action/precondition/move.test.ts
+++ src/action/precondition/move.test.ts
@@ -3,8 +3,10 @@ import { RequestType } from "../../request/requestType"
 import { Direction } from "../../room/constants"
 import { getTestPlayer } from "../../test/player"
 import { CheckStatus } from "../check"
-import move from "./move"
+import move, { MESSAGE_OUT_OF_MOVEMENT } from "./move"
 import { MESSAGE_DIRECTION_DOES_NOT_EXIST } from "./move"
+import { getTestRoom } from "../../test/room"
+import { newReciprocalExit } from "../../room/factory"
 
 describe("move", () => {
   it("should not allow movement where an exit does not exist", async () => {
@@ -14,5 +16,37 @@ describe("move", () => {
     // then
     expect(check.status).toBe(CheckStatus.Failed)
     expect(check.result).toBe(MESSAGE_DIRECTION_DOES_NOT_EXIST)
+  })
+
+  it("should not allow movement when movement points are depleted", async () => {
+    // given
+    const player = getTestPlayer()
+    player.sessionMob.vitals.mv = 0
+    const room1 = getTestRoom()
+    const room2 = getTestRoom()
+    newReciprocalExit(room1, room2, Direction.South)
+    room1.addMob(player.sessionMob)
+
+    // when
+    const check = await move(new Request(player, RequestType.South), Direction.South)
+
+    // then
+    expect(check.status).toBe(CheckStatus.Failed)
+    expect(check.result).toBe(MESSAGE_OUT_OF_MOVEMENT)
+  })
+
+  it("should allow movement when preconditions pass", async () => {
+    // given
+    const player = getTestPlayer()
+    const room1 = getTestRoom()
+    const room2 = getTestRoom()
+    newReciprocalExit(room1, room2, Direction.South)
+    room1.addMob(player.sessionMob)
+
+    // when
+    const check = await move(new Request(player, RequestType.South), Direction.South)
+
+    // then
+    expect(check.status).toBe(CheckStatus.Ok)
   })
 })
