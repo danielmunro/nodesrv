c16a6faf5ef6c661c7e933e6873edb57ec9d1c29
Dan Munro <dan@danmunro.com>
2018-07-15 09:42:46 -0500

    test case

--- src/session/auth/playerAuthStep.ts
+++ src/session/auth/playerAuthStep.ts
@@ -1,9 +1,5 @@
 import { Player } from "../../player/model/player"
 
 export default abstract class PlayerAuthStep {
-  public readonly player: Player
-
-  constructor(player: Player) {
-    this.player = player
-  }
+  constructor(public readonly player: Player) {}
 }
--- src/session/session.ts
+++ src/session/session.ts
@@ -6,6 +6,8 @@ import { createRequestArgs, Request as ActionRequest } from "../request/request"
 import { RequestType } from "../request/requestType"
 import AuthStep from "./auth/authStep"
 import Complete from "./auth/complete"
+import { default as PlayerComplete } from "./auth/createPlayer/complete"
+import { default as MobComplete } from "./auth/createMob/complete"
 import Email from "./auth/login/email"
 import Request from "./auth/request"
 import { SessionStatus } from "./status"
@@ -27,10 +29,15 @@ export default class Session {
     const response = await this.authStep.processRequest(request)
     this.authStep = response.authStep
     this.client.send({ message: response.message })
-    if (this.authStep instanceof Complete) {
-      return await this.login(this.authStep.player)
-    }
     this.client.send({ message: this.authStep.getStepMessage() })
+    if (this.authStep instanceof MobComplete
+      || this.authStep instanceof PlayerComplete || this.authStep instanceof Complete) {
+      this.authStep = (await this.authStep.processRequest(request)).authStep
+      if (this.authStep instanceof Complete) {
+        return await this.login(this.authStep.player)
+      }
+      this.client.send({ message: this.authStep.getStepMessage() })
+    }
   }
 
   public getAuthStepMessage(): string {
