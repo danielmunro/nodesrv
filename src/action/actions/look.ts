32c57576c8c3f781514f0a29c3bc7400f78f367e
Dan Munro <dan@danmunro.com>
2018-07-30 06:46:38 -0500

    extract to method

--- src/action/actions/look.ts
+++ src/action/actions/look.ts
@@ -8,25 +8,29 @@ import ResponseBuilder from "../../request/responseBuilder"
 
 export const NOT_FOUND = "You don't see that anywhere."
 
+function lookAtSubject(request, builder) {
+  const mob = request.findMobInRoom()
+  if (mob) {
+    return builder.info(mob.description)
+  }
+
+  let item = request.findItemInRoomInventory()
+  if (item) {
+    return builder.info(item.description)
+  }
+
+  item = request.findItemInSessionMobInventory()
+  if (item) {
+    return builder.info(item.description)
+  }
+
+  return builder.error(NOT_FOUND)
+}
+
 export default function(request: Request): Promise<Response> {
   const builder = new ResponseBuilder(request)
   if (request.subject) {
-    const mob = request.findMobInRoom()
-    if (mob) {
-      return builder.info(mob.description)
-    }
-
-    let item = request.findItemInRoomInventory()
-    if (item) {
-      return builder.info(item.description)
-    }
-
-    item = request.findItemInSessionMobInventory()
-    if (item) {
-      return builder.info(item.description)
-    }
-
-    return builder.error(NOT_FOUND)
+    return lookAtSubject(request, builder)
   }
 
   const room = request.getRoom()
