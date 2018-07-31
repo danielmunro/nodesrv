d816c7b26010f0eed42a2af7907092194e49bfa9
Dan Munro <dan@danmunro.com>
2018-07-31 06:50:08 -0500

    respawn to start room

--- src/server/observers/respawner.ts
+++ src/server/observers/respawner.ts
@@ -2,19 +2,22 @@ import { Client } from "../../client/client"
 import { Disposition } from "../../mob/disposition"
 import { Mob } from "../../mob/model/mob"
 import { getMobs } from "../../mob/table"
+import { findOneRoom } from "../../room/repository/room"
 import { Observer } from "./observer"
 
-function respawn(mob: Mob) {
+async function respawn(mob: Mob) {
   mob.disposition = Disposition.Standing
   const combined = mob.getCombinedAttributes()
   mob.vitals.hp = combined.vitals.hp
   mob.vitals.mana = combined.vitals.mana
   mob.vitals.mv = combined.vitals.mv
+  const startRoom = await findOneRoom(mob.startRoom.id)
+  startRoom.addMob(mob)
 }
 
 export default class Respawner implements Observer {
   public notify(clients: Client[]): void {
     getMobs().filter((mob) => mob.disposition === Disposition.Dead)
-      .forEach((mob) => respawn(mob))
+      .forEach(async (mob) => await respawn(mob))
   }
 }
