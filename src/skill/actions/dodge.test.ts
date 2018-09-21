35c5bee31edcdbb21b488c43fb067b5a76b6109f
Dan Munro <dan@danmunro.com>
2018-09-21 07:20:04 -0500

    refactor dodge test

--- src/skill/actions/dodge.test.ts
+++ src/skill/actions/dodge.test.ts
@@ -1,6 +1,6 @@
+import doNTimes from "../../functional/times"
 import { AttackResult } from "../../mob/fight/attack"
 import { Fight } from "../../mob/fight/fight"
-import { Round } from "../../mob/fight/round"
 import { getTestMob } from "../../test/mob"
 import { newSkill } from "../factory"
 import { SkillType } from "../skillType"
@@ -11,17 +11,13 @@ describe("dodge skill", () => {
     attacker.skills.push(newSkill(SkillType.Dodge, 10))
     const defender = getTestMob()
     const fight = new Fight(attacker, defender, attacker.room)
-    const results = []
+    const results = await doNTimes(20, () => fight.round())
 
-    for (let i = 0; i < 20; i++) {
-      results.push(await fight.round())
-    }
-
-    expect(results.every((r: Round) => {
+    expect(results.every(r => {
       const lastAttack = r.getLastAttack()
       return lastAttack.result === AttackResult.Hit || lastAttack.result === AttackResult.Miss
     })).toBeTruthy()
-    expect(results.some((r: Round) => r.getLastCounter().result === AttackResult.Dodge)).toBeTruthy()
-    expect(results.some((r: Round) => r.getLastCounter().result === AttackResult.Hit)).toBeTruthy()
+    expect(results.some(r => r.getLastCounter().result === AttackResult.Dodge)).toBeTruthy()
+    expect(results.some(r => r.getLastCounter().result === AttackResult.Hit)).toBeTruthy()
   })
 })
