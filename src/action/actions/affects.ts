5b6147f4a60d9fc196b5164d3b9870f2144e4f94
Dan Munro <dan@danmunro.com>
2018-09-25 21:48:32 -0500

    refactoring of actions

--- src/action/actions/affects.ts
+++ src/action/actions/affects.ts
@@ -1,15 +1,12 @@
 import { Affect } from "../../affect/model/affect"
 import { Request } from "../../request/request"
 import Response from "../../request/response"
-import ResponseBuilder from "../../request/responseBuilder"
 
 function reduceAffects(affects) {
-  return affects.reduce(
-    (previous, current: Affect) =>
-      previous + "\n" + current.affectType + ": " + current.timeout + " hour" + (current.timeout === 1 ? "" : "s"),
-    "")
+  return affects.reduce((previous, current: Affect) =>
+    previous + "\n" + current.affectType + ": " + current.timeout + " hour" + (current.timeout === 1 ? "" : "s"), "")
 }
 
 export default function(request: Request): Promise<Response> {
-  return new ResponseBuilder(request).info("Your affects:\n" + reduceAffects(request.player.sessionMob.affects))
+  return request.respondWith().info("Your affects:\n" + reduceAffects(request.mob.affects))
 }
