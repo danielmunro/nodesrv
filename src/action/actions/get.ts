c6e9fe91146e75bc3d5468748f3cd205b4d4ecd1
Dan Munro <dan@danmunro.com>
2018-09-27 05:49:25 -0500

    re-arrange lines in method

--- src/action/actions/get.ts
+++ src/action/actions/get.ts
@@ -6,12 +6,12 @@ import { Messages } from "./constants"
 
 export default function(checkedRequest: CheckedRequest): Promise<Response> {
   const item = checkedRequest.check.result
-  const mob = checkedRequest.request.mob
-  mob.inventory.addItem(item)
   const container = checkedRequest.getCheckTypeResult(CheckType.ContainerPresent)
   const message = container ?
     format(Messages.Get.SuccessFromContainer, item.name, container.name) :
     format(Messages.Get.SuccessFromRoom, item.name)
+
+  checkedRequest.request.mob.inventory.addItem(item)
 
   return checkedRequest.respondWith().success(message)
 }
