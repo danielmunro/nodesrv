19cce8c705aeb491fa7a66927d3fe75b02ca5005
Dan Munro <dan@danmunro.com>
2018-09-30 12:45:34 -0500

    fix get test

--- src/action/actions/get.test.ts
+++ src/action/actions/get.test.ts
@@ -1,8 +1,10 @@
 import { RequestType } from "../../request/requestType"
 import TestBuilder from "../../test/testBuilder"
 import get from "./get"
+import getPrecondition from "../precondition/get"
+import CheckedRequest from "../../check/checkedRequest"
 
-describe("get actions actions", () => {
+describe("get action", () => {
   it("should be able to get an item from a room inventory", async () => {
     // setup
     const testBuilder = new TestBuilder()
@@ -12,7 +14,9 @@ describe("get actions actions", () => {
     const itemCount = player.sessionMob.inventory.items.length
 
     // when
-    const response = await get(testBuilder.createOkCheckedRequest(RequestType.Get, "get cap", item))
+    const request = testBuilder.createRequest(RequestType.Get, "get cap")
+    const check = await getPrecondition(request, await testBuilder.getService())
+    const response = await get(new CheckedRequest(request, check))
 
     // then
     expect(player.sessionMob.inventory.items.length).toBe(itemCount + 1)
