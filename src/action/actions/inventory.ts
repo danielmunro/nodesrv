32848941cf383fac7eda983f90ce71c3e3068e96
Dan Munro <dan@danmunro.com>
2018-09-30 15:42:46 -0500

    superficial refactor

--- src/action/actions/inventory.ts
+++ src/action/actions/inventory.ts
@@ -5,6 +5,6 @@ import Service from "../../service/service"
 export default function(request: Request, service: Service): Promise<Response> {
   return request.respondWith()
     .info("Your inventory:\n" +
-      service.itemTable.findByInventory(request.player.getInventory())
+      service.itemTable.findByInventory(request.mob.inventory)
         .reduce((previous, current) => previous + current.name + "\n", ""))
 }
--- src/action/actions/sell.ts
+++ src/action/actions/sell.ts
@@ -4,15 +4,21 @@ import Response from "../../request/response"
 import { format } from "../../support/string"
 import { ActionOutcome } from "../actionOutcome"
 import { Messages } from "./constants"
+import { Mob } from "../../mob/model/mob"
+import { Item } from "../../item/model/item"
 
 export default function(checkedRequest: CheckedRequest): Promise<Response> {
   const item = checkedRequest.getCheckTypeResult(CheckType.HasItem)
   const mob = checkedRequest.request.mob
 
+  return checkedRequest
+    .respondWith(ActionOutcome.ItemDestroyed, item)
+    .success(sell(mob, item))
+}
+
+function sell(mob: Mob, item: Item) {
   mob.inventory.removeItem(item)
   mob.gold += item.value
 
-  return checkedRequest
-    .respondWith(ActionOutcome.ItemDestroyed, item)
-    .success(format(Messages.Sell.Success, item.name, item.value))
+  return format(Messages.Sell.Success, item.name, item.value)
 }
