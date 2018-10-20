b2fd4afe079aff633add726d1ccc0af6c24a8d72
Dan Munro <dan@danmunro.com>
2018-10-20 07:24:03 -0500

    refactor out local variables

--- src/action/precondition/remove.ts
+++ src/action/precondition/remove.ts
@@ -5,7 +5,10 @@ import { Request } from "../../request/request"
 import { MESSAGE_REMOVE_FAIL } from "./constants"
 
 export default function(request: Request): Promise<Check> {
-  const item = request.mob.equipped.inventory.findItemByName(request.getContextAsInput().subject)
-
-  return new CheckBuilder().require(item, MESSAGE_REMOVE_FAIL, CheckType.HasItem).create(item)
+  return new CheckBuilder()
+    .require(
+      request.mob.equipped.inventory.findItemByName(request.getContextAsInput().subject),
+      MESSAGE_REMOVE_FAIL,
+      CheckType.HasItem)
+    .create()
 }
--- src/action/precondition/sacrifice.ts
+++ src/action/precondition/sacrifice.ts
@@ -3,9 +3,10 @@ import { Request } from "../../request/request"
 import { MESSAGE_FAIL_CONTAINER_NOT_EMPTY, Messages } from "./constants"
 
 export default async function(request: Request): Promise<Check> {
-  const item = request.findItemInRoomInventory()
   return request.checkWithStandingDisposition()
-    .require(item, Messages.All.Item.NotFound)
-    .require(item.isContainer() ? item.containerInventory.items.length === 0 : true, MESSAGE_FAIL_CONTAINER_NOT_EMPTY)
-    .create(item)
+    .require(request.findItemInRoomInventory(), Messages.All.Item.NotFound)
+    .capture()
+    .require(item => item.isContainer()
+      ? item.containerInventory.items.length === 0 : true, MESSAGE_FAIL_CONTAINER_NOT_EMPTY)
+    .create()
 }
