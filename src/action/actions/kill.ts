1c36f14a383c25431b3a420c02c65ade3ce7997b
Dan Munro <dan@danmunro.com>
2018-10-20 07:33:26 -0500

    lint

--- src/action/actions/kill.ts
+++ src/action/actions/kill.ts
@@ -1,8 +1,5 @@
 import CheckedRequest from "../../check/checkedRequest"
-import { addFight, Fight } from "../../mob/fight/fight"
-import { Mob } from "../../mob/model/mob"
 import Response from "../../request/response"
-import { Room } from "../../room/model/room"
 import { ActionOutcome } from "../actionOutcome"
 import { Messages } from "./constants"
 
@@ -14,8 +11,4 @@ export default function(checkedRequest: CheckedRequest): Promise<Response> {
     { screamVerb: "scream", attackVerb: "attack", target: request.getTarget() },
     { screamVerb: "screams", attackVerb: "attacks", target: "you" },
   { screamVerb: "screams", attackVerb: "attacks", target: request.getTarget() })
-}
-
-function fight(attacker: Mob, defender: Mob, room: Room) {
-  addFight(new Fight(attacker, defender, room))
 }
