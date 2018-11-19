fdbb2f7149cbfff0ed968d48a2ea7318d3dc09db
Dan Munro <dan@danmunro.com>
2018-11-19 09:18:02 -0600

    missed new file

---
+++ src/import/resetMaterializer.ts
@@ -0,0 +1,25 @@
+import ItemRepository from "../item/repository/item"
+import ItemResetRepository from "../item/repository/itemReset"
+import { newMobReset } from "../mob/factory"
+import MobRepository from "../mob/repository/mob"
+import MobResetRepository from "../mob/repository/mobReset"
+import RoomRepository from "../room/repository/room"
+import File from "./file"
+
+export default class ResetMaterializer {
+  constructor(
+    public readonly mobResetRepository: MobResetRepository,
+    public readonly itemResetRepository: ItemResetRepository,
+    public readonly mobRepository: MobRepository,
+    public readonly itemRepository: ItemRepository,
+    public readonly roomRepository: RoomRepository,
+  ) {}
+
+  public async materializeResets(file: File) {
+    await Promise.all(file.mobResets.map(async resetObject => {
+      const room = await this.roomRepository.findOneByImportId(resetObject.roomId)
+      const mob = await this.mobRepository.findOneByImportId(resetObject.mobId)
+      await this.mobResetRepository.save(newMobReset(mob, room))
+    }))
+  }
+}
