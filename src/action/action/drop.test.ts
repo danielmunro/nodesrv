fa34945acb69658aebf19c8177580fe23d819947
Dan Munro <dan@danmunro.com>
2018-11-21 22:47:30 -0600

    nodrop

--- src/action/action/drop.test.ts
+++ src/action/action/drop.test.ts
@@ -1,24 +1,37 @@
 import { RequestType } from "../../request/requestType"
 import TestBuilder from "../../test/testBuilder"
 import drop from "./drop"
+import getActionCollection from "../actionCollection"
+import { Definition } from "../definition/definition"
+import { Room } from "../../room/model/room"
+import { Mob } from "../../mob/model/mob"
+import { Equipment } from "../../item/equipment"
+import { Item } from "../../item/model/item"
+import { AffectType } from "../../affect/affectType"
+import { newAffect } from "../../affect/factory"
+import ResponseAction from "../../request/responseAction"
+
+let testBuilder: TestBuilder
+let actionDefinition: Definition
+let room: Room
+let mob: Mob
+let equipment: Item
+
+beforeEach(async () => {
+  testBuilder = new TestBuilder()
+  room = testBuilder.withRoom().room
+  const playerBuilder = await testBuilder.withPlayer()
+  mob = playerBuilder.player.sessionMob
+  equipment = playerBuilder.withHelmetEq()
+  const actionCollection = getActionCollection(await testBuilder.getService())
+  actionDefinition = actionCollection.getMatchingHandlerDefinitionForRequestType(RequestType.Drop)
+})
 
 describe("drop", () => {
   it("should be able to drop an item", async () => {
-    // given
-    const testBuilder = new TestBuilder()
-    const room = testBuilder.withRoom().room
-    const playerBuilder = await testBuilder.withPlayer()
-    const mob = playerBuilder.player.sessionMob
-
-    // and
-    const equipment = playerBuilder.withHelmetEq()
-
     // when
-    const response = await drop(
-      testBuilder.createOkCheckedRequest(
-        RequestType.Drop,
-        "drop cap",
-        equipment))
+    const response = await actionDefinition.handle(
+      testBuilder.createRequest(RequestType.Drop, "drop cap", equipment))
 
     // then
     const message = response.message.getMessageToRequestCreator()
@@ -26,5 +39,20 @@ describe("drop", () => {
     expect(message).toContain(equipment.name)
     expect(room.inventory.items).toHaveLength(1)
     expect(mob.inventory.items).toHaveLength(0)
+  })
+
+  it("an item with MeltDrop affect should destroy on drop", async () => {
+    // given
+    equipment.affects.push(newAffect(AffectType.MeltDrop))
+
+    // when
+    const response = await actionDefinition.handle(
+      testBuilder.createRequest(RequestType.Drop, "drop cap", equipment))
+
+    // then
+    const message = response.message.getMessageToRequestCreator()
+    expect(message).toContain("you drop")
+    expect(message).toContain(equipment.name)
+    expect(response.responseAction.wasItemDestroyed()).toBeTruthy()
   })
 })
--- src/action/action/drop.ts
+++ src/action/action/drop.ts
@@ -1,14 +1,22 @@
 import CheckedRequest from "../../check/checkedRequest"
 import Response from "../../request/response"
 import { Messages } from "./constants"
+import { Item } from "../../item/model/item"
+import { AffectType } from "../../affect/affectType"
+import { ActionOutcome } from "../actionOutcome"
 
 export default function(checkedRequest: CheckedRequest): Promise<Response> {
-  const item = checkedRequest.check.result
+  const item = checkedRequest.check.result as Item
   const room = checkedRequest.request.getRoom()
+  let actionOutcome = null
 
-  room.inventory.addItem(item)
+  if (item.affects.find(a => a.affectType === AffectType.MeltDrop)) {
+    actionOutcome = ActionOutcome.ItemDestroyed
+  } else {
+    room.inventory.addItem(item)
+  }
 
-  return checkedRequest.respondWith().success(
+  return checkedRequest.respondWith(actionOutcome, item).success(
     Messages.Drop.Success,
     { item, verb: "drop" },
     { item, verb: "drops" })
