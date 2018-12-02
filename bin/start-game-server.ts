3015672afb0df06841a768dd3fa5e794c92f5427
Dan Munro <dan@danmunro.com>
2018-12-01 20:26:48 -0600

    logging/timing

--- bin/start-game-server.ts
+++ bin/start-game-server.ts
@@ -21,18 +21,65 @@ import { default as RoomTable } from "../src/room/roomTable"
 import newServer from "../src/server/factory"
 import { initializeConnection } from "../src/support/db/connection"
 
+const Timings = {
+  exitTable: "exit table initialization",
+  init: "total game initialization",
+  openPort: "open server port",
+  resetService: "create reset service initialization",
+  roomAndMobTables: "room and mob table initialization",
+  seedWorld: "seeding world",
+}
+
 /**
  * Obtain the start room ID and port from arguments passed in
  */
 const startRoomID = process.argv[2]
 const port = +process.argv[3]
-
+console.time(Timings.init)
 assert.ok(startRoomID, "start room ID is required to be defined")
-console.info("0 - entry point", { startRoomID })
+const locationService = new LocationService()
+console.log(`startup parameters:  port: ${port}, room: ${startRoomID}`)
+
+createDbConnection().then(async () => {
+  console.time(Timings.roomAndMobTables)
+  const tables = await Promise.all([
+    await newRoomTable(),
+    await createMobService(new MobTable(), locationService),
+  ])
+  const roomTable = tables[0]
+  const mobService = tables[1]
+  console.debug(`room table initialized with ${roomTable.count()} rooms`)
+  console.timeEnd(Timings.roomAndMobTables)
+
+  console.time(Timings.resetService)
+  const itemService = await new ItemService(await getItemRepository(), new ItemTable())
+  const resetService = await createResetService(mobService, roomTable, itemService)
+  console.timeEnd(Timings.resetService)
+
+  console.time(Timings.seedWorld)
+  const results = await Promise.all([
+    newExitTable(locationService),
+    resetService.seedMobTable(),
+    resetService.seedItemRoomResets()])
+  console.timeEnd(Timings.seedWorld)
+
+  console.time(Timings.exitTable)
+  const exitTable = results[0]
+  console.timeEnd(Timings.exitTable)
+
+  console.time(Timings.openPort)
+  const gameService = await GameService.new(mobService, itemService, roomTable, exitTable)
+  await startServer(
+    gameService,
+    roomTable.get(startRoomID),
+    resetService,
+    mobService)
+  console.timeEnd(Timings.openPort)
+  console.timeEnd(Timings.init)
+})
 
 async function startServer(
   service: GameService, startRoom: Room, resetService: ResetService, mobService: MobService) {
-  console.info(`3 - starting up server on port ${port}`)
   return (await newServer(service, port, startRoom, resetService, mobService)).start()
 }
 
@@ -45,14 +92,11 @@ async function newRoomTable(): Promise<RoomTable> {
 async function newExitTable(service: LocationService): Promise<ExitTable> {
   const exitRepository = await getExitRepository()
   const models = await exitRepository.findAll()
-  console.log(`2 - exit table initialized with ${models.length} exits`)
-
   return new ExitTable(service, models)
 }
 
 async function createDbConnection(): Promise<void> {
   await initializeConnection()
-  console.debug("1 - database connection created")
 }
 
 async function createResetService(
@@ -71,24 +115,6 @@ async function createResetService(
     roomTable,
     itemService)
 }
-
-const locationService = new LocationService([])
-createDbConnection().then(async () => {
-  const roomTable = await newRoomTable()
-  console.debug(`2 - room table initialized with ${roomTable.count()} rooms`)
-  const mobService = await createMobService(new MobTable(), locationService)
-  const itemService = await new ItemService(await getItemRepository(), new ItemTable())
-  const resetService = await createResetService(mobService, roomTable, itemService)
-  console.log("3 - seeding world")
-  await resetService.seedMobTable()
-  await resetService.seedItemRoomResets()
-  console.log(`4 - creating server on port ${port}`)
-  await startServer(
-    await GameService.new(mobService, itemService, roomTable, await newExitTable(locationService)),
-    roomTable.get(startRoomID),
-    resetService,
-    mobService)
-})
 
 async function createMobService(mobTable: MobTable, aLocationService: LocationService) {
   return new MobService(mobTable, await getMobRepository(), new FightTable(), aLocationService)
--- src/server/observers/tick.ts
+++ src/server/observers/tick.ts
@@ -7,6 +7,7 @@ import { createSkillTriggerEvent } from "../../skill/trigger/factory"
 import { Observer } from "./observer"
 
 const MESSAGE_HUNGRY = "You are hungry."
+const TIMING = "tick notification duration"
 
 export class Tick implements Observer {
   constructor(
@@ -14,15 +15,15 @@ export class Tick implements Observer {
     private readonly locationService: LocationService) {}
 
   public async notify(clients: Client[]): Promise<void> {
+    console.time(TIMING)
     const id = v4()
     const timestamp = new Date()
     this.service.incrementTime()
-
     await Promise.all(clients
       .filter(client => client.isLoggedIn())
       .map(client => this.notifyClient(client, id, timestamp)))
-
     console.log(`tick at ${timestamp}`)
+    console.timeEnd(TIMING)
   }
 
   private async notifyClient(client: Client, id: string, timestamp: Date) {
