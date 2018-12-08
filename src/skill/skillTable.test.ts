e2071e9bf50c86f8e0b6dbac17dff1bb8bd9ebd0
Dan Munro <dan@danmunro.com>
2018-12-08 09:00:56 -0600

    test name

--- src/skill/skillTable.test.ts
+++ src/skill/skillTable.test.ts
@@ -1,12 +1,20 @@
 import TestBuilder from "../test/testBuilder"
 import {SkillType} from "./skillType"
 import {RequestType} from "../request/requestType"
+import {ActionOutcome} from "../action/actionOutcome"
 
 describe("skill table", () => {
-  it("should do something weapony", async () => {
+  it("should be able to invoke a weapon skill", async () => {
+    // setup
     const testBuilder = new TestBuilder()
+
+    // given
     const skill = await testBuilder.getSkillDefinition(SkillType.Axe)
+
+    // when
     const response = await skill.doAction(testBuilder.createRequest(RequestType.Noop))
-    console.log(response)
+
+    // then
+    expect(response.responseAction.actionOutcome).toBe(ActionOutcome.None)
   })
 })
