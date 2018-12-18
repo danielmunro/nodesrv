a4de3068ec7f6d85e8645e84cb80cbeaf957f5cb
Dan Munro <dan@danmunro.com>
2018-12-18 05:46:41 -0600

    open should require arguments

--- src/action/action/open.test.ts
+++ src/action/action/open.test.ts
@@ -4,8 +4,19 @@ import {Direction} from "../../room/constants"
 import Door from "../../room/model/door"
 import TestBuilder from "../../test/testBuilder"
 import getActionCollection from "../actionCollection"
+import {Messages} from "../precondition/constants"
 
 describe("open action", () => {
+  it("should require arguments", async () => {
+    const testBuilder = new TestBuilder()
+    const service = await testBuilder.getService()
+    const actionCollection = getActionCollection(service)
+    const definition = actionCollection.getMatchingHandlerDefinitionForRequestType(RequestType.Open)
+    const response = await definition.handle(testBuilder.createRequest(RequestType.Open, "open"))
+
+    expect(response.message.getMessageToRequestCreator()).toBe(Messages.All.Arguments.Open)
+  })
+
   it("should be able to open doors", async () => {
     // setup
     const testBuilder = new TestBuilder()
