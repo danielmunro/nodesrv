d6a7d534a382bdba0f05267e0e91702cb981799e
Dan Munro <dan@danmunro.com>
2018-12-17 19:57:12 -0600

    blue: exit/refactor

--- src/room/model/exit.ts
+++ src/room/model/exit.ts
@@ -1,18 +1,23 @@
-import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm"
+import {Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn} from "typeorm"
 import { Direction } from "../constants"
 import { Room } from "./room"
+import Door from "./door"
 
 @Entity()
 export class Exit {
-    @PrimaryGeneratedColumn()
-    public id: number
+  @PrimaryGeneratedColumn()
+  public id: number
 
-    @Column("text")
-    public direction: Direction
+  @Column("text")
+  public direction: Direction
 
-    @ManyToOne((type) => Room, (room) => room.exits)
-    public source: Room
+  @ManyToOne(() => Room, (room) => room.exits)
+  public source: Room
 
-    @ManyToOne((type) => Room)
-    public destination: Room
+  @ManyToOne(() => Room)
+  public destination: Room
+
+  @OneToOne(() => Door, { eager: true })
+  @JoinColumn()
+  public door: Door
 }
