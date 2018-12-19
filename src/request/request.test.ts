3c86d259808c61d9ca30e3967bf23e4a84a9349a
Dan Munro <dan@danmunro.com>
2018-12-19 07:44:21 -0600

    more comprehensive test

--- src/request/request.test.ts
+++ src/request/request.test.ts
@@ -1,18 +1,18 @@
 import { Equipment } from "../item/equipment"
-import { Item } from "../item/model/item"
 import TestBuilder from "../test/testBuilder"
 import { RequestType } from "./requestType"
+import {newEquipment} from "../item/factory"
 
 describe("request", () => {
   it("should be able to find an item in a request session mob's inventory", async () => {
     const testBuilder = new TestBuilder()
     const playerBuilder = await testBuilder.withPlayer()
     const player = playerBuilder.player
-    const item = new Item()
-    item.name = "a cracked wooden practice shield"
-    item.equipment = Equipment.Shield
-    player.getInventory().addItem(item)
+    const item1 = newEquipment("a steel practice shield", "description", Equipment.Shield)
+    const item2 = newEquipment("a wooden practice shield", "description", Equipment.Shield)
+    player.getInventory().addItem(item1)
+    player.getInventory().addItem(item2)
     expect(testBuilder.createRequest(RequestType.Wear, "wear floodle").findItemInSessionMobInventory()).toBeFalsy()
-    expect(testBuilder.createRequest(RequestType.Wear, "wear practice").findItemInSessionMobInventory()).toBe(item)
+    expect(testBuilder.createRequest(RequestType.Wear, "wear 2.practice").findItemInSessionMobInventory()).toBe(item2)
   })
 })
