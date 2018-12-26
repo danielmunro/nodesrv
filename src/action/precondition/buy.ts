f162d1eb98df6eceec48dea2df2bcad5be552850
Dan Munro <dan@danmunro.com>
2018-12-26 06:34:04 -0600

    refactor buy preconditions

--- src/action/precondition/buy.ts
+++ src/action/precondition/buy.ts
@@ -1,27 +1,19 @@
 import Check from "../../check/check"
 import CheckBuilder from "../../check/checkBuilder"
 import GameService from "../../gameService/gameService"
-import { Request } from "../../request/request"
-import { MESSAGE_ERROR_NO_ITEM, Messages } from "./constants"
+import {Request} from "../../request/request"
+import {MESSAGE_ERROR_NO_ITEM, Messages} from "./constants"
+import {CheckType} from "../../check/checkType"
 
 export default function(request: Request, service: GameService): Promise<Check> {
   const subject = request.getContextAsInput().subject
   const merchant = service.getMobsByRoom(request.room).find(mob => mob.isMerchant())
-  let item
 
-  const checkBuilder = new CheckBuilder(service.mobService)
+  return new CheckBuilder(service.mobService)
     .require(subject, Messages.All.Arguments.Buy)
     .requireMob(merchant, Messages.All.Item.NoMerchant)
-
-  if (merchant) {
-    item = merchant.inventory.findItemByName(subject)
-    checkBuilder.require(item, MESSAGE_ERROR_NO_ITEM)
-    checkBuilder.capture()
-
-    if (item) {
-      checkBuilder.require(request.mob.gold > item.value, Messages.Buy.CannotAfford)
-    }
-  }
-
-  return checkBuilder.create()
+    .require(mob => mob.inventory.findItemByName(subject), MESSAGE_ERROR_NO_ITEM, CheckType.HasItem)
+    .capture()
+    .require(item => request.mob.gold > item.value, Messages.Buy.CannotAfford)
+    .create()
 }
