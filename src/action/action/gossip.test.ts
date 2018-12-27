b2b9415461157da43f5b54e56f55434a26dd8b69
Dan Munro <dan@danmunro.com>
2018-12-26 22:30:10 -0600

    refactor gossip tests

--- src/action/action/gossip.test.ts
+++ src/action/action/gossip.test.ts
@@ -1,17 +1,46 @@
 import { RequestType } from "../../request/requestType"
 import TestBuilder from "../../test/testBuilder"
+import gossip from "./gossip"
+import CheckedRequest from "../../check/checkedRequest"
+import { Request } from "../../request/request"
+import {getTestMob} from "../../test/mob"
+import {getTestRoom} from "../../test/room"
+import InputContext from "../../request/context/inputContext"
+import Check from "../../check/check"
+import SocialEvent from "../../client/event/socialEvent"
 
-it("should be to handle gossiping", async () => {
-  // setup
-  const testBuilder = new TestBuilder()
-  await testBuilder.withPlayer()
-  const actions = await testBuilder.getActionCollection()
-  const request = testBuilder.createRequest(RequestType.Gossip, "gossip hello world")
-  const handler = actions.getMatchingHandlerDefinitionForRequestType(request.getType())
+describe("gossip", () => {
+  it("should be to handle gossiping", async () => {
+    // setup
+    const testBuilder = new TestBuilder()
+    await testBuilder.withPlayer()
+    const actions = await testBuilder.getActionCollection()
+    const request = testBuilder.createRequest(RequestType.Gossip, "gossip hello world")
+    const handler = actions.getMatchingHandlerDefinitionForRequestType(request.getType())
 
-  // when
-  const response = await handler.handle(request)
+    // when
+    const response = await handler.handle(request)
 
-  // then
-  expect(response.message.getMessageToRequestCreator()).toEqual("You gossip, \"hello world\"")
+    // then
+    expect(response.message.getMessageToRequestCreator()).toEqual("You gossip, \"hello world\"")
+  })
+
+  it("should publish a social event", async () => {
+    // setup
+    const service = jest.fn(() => ({
+      publishEvent: jest.fn(),
+    }))()
+
+    // when
+    await gossip(new CheckedRequest(
+      new Request(
+        getTestMob(),
+        getTestRoom(),
+        new InputContext(RequestType.Gossip, "gossip hello")),
+      await Check.ok()), service)
+
+    // then
+    expect(service.publishEvent.mock.calls.length).toBe(1)
+    expect(service.publishEvent.mock.calls[0][0]).toBeInstanceOf(SocialEvent)
+  })
 })
