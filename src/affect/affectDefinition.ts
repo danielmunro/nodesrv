8a5ebe635b5569f323705a2736ad1ac85a1a09b5
Dan Munro <dan@danmunro.com>
2019-01-09 06:31:33 -0600

    add stack message

--- src/affect/affectDefinition.ts
+++ src/affect/affectDefinition.ts
@@ -2,5 +2,8 @@ import {AffectType} from "./affectType"
 import {StackBehavior} from "./stackBehavior"
 
 export default class AffectDefinition {
-  constructor(public readonly affectType: AffectType, public readonly stackBehavior: StackBehavior) {}
+  constructor(
+    public readonly affectType: AffectType,
+    public readonly stackBehavior: StackBehavior,
+    public readonly stackMessage: string = null) {}
 }
--- src/affect/affectTable.ts
+++ src/affect/affectTable.ts
@@ -1,7 +1,13 @@
 import AffectDefinition from "./affectDefinition"
 import {AffectType} from "./affectType"
 import {StackBehavior} from "./stackBehavior"
+import {Messages} from "../spell/precondition/constants"
 
 export default [
   new AffectDefinition(AffectType.Berserk, StackBehavior.NoReplace),
+  new AffectDefinition(AffectType.Shield, StackBehavior.Replace),
+  new AffectDefinition(AffectType.GiantStrength, StackBehavior.Replace),
+  new AffectDefinition(AffectType.Poison, StackBehavior.Replace),
+  new AffectDefinition(AffectType.Curse, StackBehavior.NoReplace),
+  new AffectDefinition(AffectType.Blind, StackBehavior.NoReplace, Messages.Blind.AlreadyBlind),
 ]
