1eb3d2978ca322c3f33c4aba5f57b79aae688fb6
Dan Munro <dan@danmunro.com>
2019-02-10 07:08:33 -0600

    add deployment step

---
+++ bin/deploy.sh
@@ -0,0 +1,22 @@
+# install AWS SDK
+sudo pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# install necessary dependency for ecs-deploy
+sudo apt-get install jq -y
+
+# install ecs-deploy
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
+  sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+# login AWS ECR
+eval $(aws ecr get-login --region us-west-2)
+
+# build the docker image and push to an image repository
+docker build -t nodesrv-app .
+docker tag nodesrv-app:latest $IMAGE_REPO
+docker push $IMAGE_REPO
+
+# update an AWS ECS service with the new image
+ecs-deploy -c nodesrv-app -n app-2 -i $IMAGE_REPO
