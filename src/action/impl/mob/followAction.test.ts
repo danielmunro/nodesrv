b3e3ee97514a7bab3fedc0935635871f73c7d300
Dan Munro <dan@danmunro.com>
2019-02-21 07:19:12 -0600

    add follow action

---
+++ src/action/impl/mob/followAction.test.ts
@@ -0,0 +1,29 @@
+import {Mob} from "../../../mob/model/mob"
+import {Player} from "../../../player/model/player"
+import {RequestType} from "../../../request/requestType"
+import TestBuilder from "../../../test/testBuilder"
+import Action from "../../action"
+
+let testBuilder: TestBuilder
+let action: Action
+let player: Player
+let target: Mob
+
+beforeEach(async () => {
+  testBuilder = new TestBuilder()
+  action = await testBuilder.getActionDefinition(RequestType.Follow)
+  player = (await testBuilder.withPlayer()).player
+  target = testBuilder.withMob().mob
+})
+
+describe("sell action", () => {
+  it("should be able to work successfully", async () => {
+    // when
+    const response = await action.handle(testBuilder.createRequest(RequestType.Follow, `follow '${target.name}'`))
+
+    // then
+    expect(response.message.getMessageToRequestCreator()).toBe(`you begin following ${target.name}.`)
+    expect(response.message.getMessageToTarget()).toBe(`${player.sessionMob.name} begins following you.`)
+    expect(response.message.getMessageToObservers()).toBe(`${player.sessionMob.name} begins following ${target.name}.`)
+  })
+})
---
+++ src/action/impl/mob/followAction.ts
@@ -0,0 +1,46 @@
+import Check from "../../../check/check"
+import CheckBuilderFactory from "../../../check/checkBuilderFactory"
+import CheckedRequest from "../../../check/checkedRequest"
+import {CheckType} from "../../../check/checkType"
+import {Request} from "../../../request/request"
+import {RequestType} from "../../../request/requestType"
+import Response from "../../../request/response"
+import Action from "../../action"
+import {Messages} from "../../constants"
+import {ActionPart} from "../../enum/actionPart"
+
+export default class FollowAction extends Action {
+  constructor(
+    private readonly checkBuilderFactory: CheckBuilderFactory) {
+    super()
+  }
+
+  public check(request: Request): Promise<Check> {
+    return this.checkBuilderFactory.createCheckBuilder(request)
+      .requireMob()
+      .create()
+  }
+
+  public getActionParts(): ActionPart[] {
+    return [ActionPart.Action, ActionPart.Target]
+  }
+
+  /* istanbul ignore next */
+  public getHelpText(): string {
+    return Messages.Help.NoActionHelpTextProvided
+  }
+
+  public getRequestType(): RequestType {
+    return RequestType.Follow
+  }
+
+  public invoke(checkedRequest: CheckedRequest): Promise<Response> {
+    const target = checkedRequest.getCheckTypeResult(CheckType.HasTarget)
+    checkedRequest.mob.follows = target
+    return checkedRequest.respondWith().success(
+      Messages.Follow.Success,
+      { verb: "begin", target },
+      { verb: "begins", target: "you" },
+      { verb: "begins", target })
+  }
+}
