150afc878efc4bde6da208ea2b955510d27ac460
Dan Munro <dan@danmunro.com>
2019-03-10 08:41:21 -0500

    remove duplication/clean up

---
+++ src/gameService/constants.ts
@@ -0,0 +1,110 @@
+export const PracticeMessages = {
+  Cleric: `skill                   current practiced level
+
+level 1
+cure light              1
+
+level 5
+bless                   NA
+
+level 7
+cure serious            NA
+
+level 13
+cure poison             NA
+
+level 20
+heal                    NA
+shield                  NA
+
+level 25
+stone skin              NA
+`,
+  Mage: `skill                   current practiced level
+
+level 1
+magic missile           1
+
+level 11
+giant strength          NA
+
+level 12
+blind                   NA
+
+level 13
+lightning bolt          NA
+
+level 17
+poison                  NA
+
+level 18
+curse                   NA
+
+level 20
+detect invisibility     NA
+
+level 25
+invisibility            NA
+
+level 30
+wrath                   NA
+`,
+  Ranger: `skill                   current practiced level
+
+level 1
+dodge                   1
+backstab                1
+
+level 3
+dirt kick               NA
+
+level 4
+sneak                   NA
+
+level 5
+sharpen                 NA
+steal                   NA
+
+level 12
+second attack           NA
+
+level 15
+envenom                 NA
+
+level 16
+fast healing            NA
+
+level 25
+enhanced damage         NA
+
+level 31
+hamstring               NA
+`,
+  Warrior: `skill                   current practiced level
+
+level 1
+bash                    1
+berserk                 1
+
+level 3
+trip                    NA
+
+level 5
+enhanced damage         NA
+
+level 6
+fast healing            NA
+
+level 7
+second attack           NA
+
+level 11
+disarm                  NA
+
+level 13
+dodge                   NA
+
+level 18
+shield bash             NA
+`,
+}
--- src/gameService/practiceService.test.ts
+++ src/gameService/practiceService.test.ts
@@ -1,6 +1,7 @@
 import {SpecializationType} from "../mob/specialization/specializationType"
 import TestBuilder from "../test/testBuilder"
 import PracticeService from "./practiceService"
+import {PracticeMessages} from "./constants"
 
 let testBuilder: TestBuilder
 const practiceService = new PracticeService()
@@ -10,151 +11,18 @@ beforeEach(() => {
 })
 
 describe("practice service", () => {
-  it("generates accurate practice messages for a warrior", () => {
+  it.each([
+    [SpecializationType.Warrior, PracticeMessages.Warrior],
+    [SpecializationType.Ranger, PracticeMessages.Ranger],
+    [SpecializationType.Mage, PracticeMessages.Mage],
+    [SpecializationType.Cleric, PracticeMessages.Cleric],
+  ])("generates accurate practice messages for a %s", (specialization: SpecializationType, message: string) => {
     // when
     const mob = testBuilder.withMob()
-      .setSpecialization(SpecializationType.Warrior)
+      .setSpecialization(specialization)
       .build()
 
     // then
-    expect(practiceService.generateOutputStatus(mob)).toBe(
-`skill                   current practiced level
-
-level 1
-bash                    1
-berserk                 1
-
-level 3
-trip                    NA
-
-level 5
-enhanced damage         NA
-
-level 6
-fast healing            NA
-
-level 7
-second attack           NA
-
-level 11
-disarm                  NA
-
-level 13
-dodge                   NA
-
-level 18
-shield bash             NA
-`)
-  })
-
-  it("generates accurate practice messages for a cleric", () => {
-    // when
-    const mob = testBuilder.withMob()
-      .setSpecialization(SpecializationType.Cleric)
-      .build()
-
-    // then
-    expect(practiceService.generateOutputStatus(mob)).toBe(
-`skill                   current practiced level
-
-level 1
-cure light              1
-
-level 5
-bless                   NA
-
-level 7
-cure serious            NA
-
-level 13
-cure poison             NA
-
-level 20
-heal                    NA
-shield                  NA
-
-level 25
-stone skin              NA
-`)
-  })
-
-  it("generates accurate practice messages for a ranger", () => {
-    // when
-    const mob = testBuilder.withMob()
-      .setSpecialization(SpecializationType.Ranger)
-      .build()
-
-    // then
-    expect(practiceService.generateOutputStatus(mob)).toBe(
-`skill                   current practiced level
-
-level 1
-dodge                   1
-backstab                1
-
-level 3
-dirt kick               NA
-
-level 4
-sneak                   NA
-
-level 5
-sharpen                 NA
-steal                   NA
-
-level 12
-second attack           NA
-
-level 15
-envenom                 NA
-
-level 16
-fast healing            NA
-
-level 25
-enhanced damage         NA
-
-level 31
-hamstring               NA
-`)
-  })
-
-  it("generates accurate practice messages for a mage", () => {
-    // when
-    const mob = testBuilder.withMob()
-      .setSpecialization(SpecializationType.Mage)
-      .build()
-
-    // then
-    expect(practiceService.generateOutputStatus(mob)).toBe(
-`skill                   current practiced level
-
-level 1
-magic missile           1
-
-level 11
-giant strength          NA
-
-level 12
-blind                   NA
-
-level 13
-lightning bolt          NA
-
-level 17
-poison                  NA
-
-level 18
-curse                   NA
-
-level 20
-detect invisibility     NA
-
-level 25
-invisibility            NA
-
-level 30
-wrath                   NA
-`)
+    expect(practiceService.generateOutputStatus(mob)).toBe(message)
   })
 })
