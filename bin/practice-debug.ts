c357e83facb198e145c8fd5bd1ed38173c66c6dd
Dan Munro <dan@danmunro.com>
2019-03-10 08:32:23 -0500

    new command to sample practice tables

---
+++ bin/practice-debug.ts
@@ -0,0 +1,21 @@
+import PracticeService from "../src/gameService/practiceService"
+import ServiceBuilder from "../src/gameService/serviceBuilder"
+import {Mob} from "../src/mob/model/mob"
+import {Race} from "../src/mob/race/race"
+import {SpecializationType} from "../src/mob/specialization/specializationType"
+import MobBuilder from "../src/test/mobBuilder"
+
+// arguments
+const race = process.argv[2] as Race
+const specialization = process.argv[3] as SpecializationType
+const level = +process.argv[4]
+
+// service & mob
+const serviceBuilder = new ServiceBuilder()
+const mob = new MobBuilder(new Mob(), serviceBuilder)
+  .setLevel(level)
+  .setRace(race)
+  .setSpecialization(specialization)
+  .build()
+const practiceService = new PracticeService()
+console.log(practiceService.generateOutputStatus(mob))
