8c70dbd00728813adb3dcdbafd6806bcf9fb3ff1
Dan Munro <dan@danmunro.com>
2019-03-22 22:00:03 -0500

    unused

--- src/action/offensiveSpell.ts
+++
@@ -1,7 +0,0 @@
-import CheckedRequest from "../check/checkedRequest"
-import {DamageType} from "../damage/damageType"
-
-export default interface OffensiveSpell {
-  getDamageType(): DamageType
-  calculateBaseDamage(checkedRequest: CheckedRequest): number
-}
