416b5f45f64299a1bb2160e2677aa90217a8effc
Dan Munro <dan@danmunro.com>
2019-03-30 06:48:23 -0500

    add location check in wander, some mobs not getting location?

--- src/server/observers/wander.ts
+++ src/server/observers/wander.ts
@@ -11,6 +11,10 @@ export class Wander implements Observer {
   public async notify(): Promise<any> {
     for (const mob of this.mobs) {
       const location = this.locationService.getLocationForMob(mob)
+      if (!location) {
+        console.log(`location not found for mob ${mob.name} :: ${mob.uuid}`)
+        continue
+      }
       const destination = pickOne(location.room.exits).destination
       await this.locationService.updateMobLocation(mob, destination)
     }
