9c1eb986d9b0ad1ba5dc2a99b08794221e9f89fb
Dan Munro <dan@danmunro.com>
2019-04-06 14:55:27 -0500

    new file

---
+++ src/affect/affectService.ts
@@ -0,0 +1,27 @@
+import {Mob} from "../mob/model/mob"
+import {AffectType} from "./affectType"
+import {Affect} from "./model/affect"
+
+export default class AffectService {
+  constructor(private readonly mob: Mob) {}
+
+  public canDetectInvisible(): boolean {
+    return this.mob.affects.find(a => a.affectType === AffectType.DetectInvisible) !== undefined
+  }
+
+  public has(affectType: AffectType): boolean {
+    return !!this.get(affectType)
+  }
+
+  public get(affectType: AffectType): Affect | undefined {
+    return this.mob.affects.find(affect => affect.affectType === affectType)
+  }
+
+  public add(affect: Affect) {
+    const current = this.has(affect.affectType)
+    if (!current) {
+      this.mob.affects.push(affect)
+      affect.mob = this.mob
+    }
+  }
+}
