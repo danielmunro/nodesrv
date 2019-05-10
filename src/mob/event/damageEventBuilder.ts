f4f14cf52acc2a57c35612e68920ef43ce37e9c0
Dan Munro <dan@danmunro.com>
2019-05-09 19:12:44 -0500

    missed new file

---
+++ src/mob/event/damageEventBuilder.ts
@@ -0,0 +1,27 @@
+import {DamageType} from "../../damage/damageType"
+import {Mob} from "../model/mob"
+import DamageEvent from "./damageEvent"
+
+export default class DamageEventBuilder {
+  private modifier: number = 1
+  private source: Mob
+
+  constructor(
+    private readonly target: Mob,
+    private readonly amount: number,
+    private readonly damageType: DamageType) {}
+
+  public setSource(source: Mob): DamageEventBuilder {
+    this.source = source
+    return this
+  }
+
+  public build(): DamageEvent {
+    return new DamageEvent(
+      this.target,
+      this.amount,
+      this.damageType,
+      this.modifier,
+      this.source)
+  }
+}
