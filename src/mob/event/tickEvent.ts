7efbbecabdcc7d81247fa89bbc4dd38296d037f5
Dan Munro <dan@danmunro.com>
2019-05-19 11:28:06 -0500

    add missed new class

---
+++ src/mob/event/tickEvent.ts
@@ -0,0 +1,8 @@
+import Event from "../../event/event"
+import {Room} from "../../room/model/room"
+import {Mob} from "../model/mob"
+
+export default interface TickEvent extends Event {
+  readonly mob: Mob
+  readonly room: Room
+}
