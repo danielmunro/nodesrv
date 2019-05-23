809de7b4ee6f35c4f7a40aaf7e484b63d029b3bf
Dan Munro <dan@danmunro.com>
2019-05-23 07:16:07 -0500

    refactor magic missile

--- src/action/impl/spell/attack/magicMissileAction.ts
+++ src/action/impl/spell/attack/magicMissileAction.ts
@@ -1,35 +1,17 @@
 import AbilityService from "../../../../check/abilityService"
-import DelayCost from "../../../../check/cost/delayCost"
-import ManaCost from "../../../../check/cost/manaCost"
-import {CheckType} from "../../../../check/enum/checkType"
-import {SpellMessages} from "../../../../spell/constants"
+import {DamageType} from "../../../../mob/fight/enum/damageType"
+import {SpecializationType} from "../../../../mob/specialization/enum/specializationType"
 import {SpellType} from "../../../../spell/spellType"
 import roll from "../../../../support/random/dice"
-import SpellBuilder from "../../../builder/spellBuilder"
-import {ActionType} from "../../../enum/actionType"
 import Spell from "../../spell"
-
-function calculateBaseDamage(): number {
-  return roll(1, 4)
-}
+import attackAction from "./attackAction"
 
 export default function(abilityService: AbilityService): Spell {
-  return new SpellBuilder(abilityService)
-    .setSpellType(SpellType.MagicMissile)
-    .setActionType(ActionType.Offensive)
-    .setCosts([
-      new ManaCost(15),
-      new DelayCost(1),
-    ])
-    .setApplySpell(async requestService => {
-      const target = requestService.getResult(CheckType.HasTarget)
-      target.vitals.hp -= calculateBaseDamage()
-    })
-    .setSuccessMessage(requestService =>
-      requestService.createResponseMessage(SpellMessages.MagicMissile.Success)
-        .setVerbToRequestCreator("is")
-        .setVerbToTarget("are")
-        .setVerbToObservers("is")
-        .create())
-    .create()
+  return attackAction(
+    abilityService,
+    SpellType.MagicMissile,
+    DamageType.Energy,
+    (_, specializationType) =>
+      roll(1, 4 + (specializationType === SpecializationType.Mage ? 1 : 0)),
+    "magic missile")
 }
--- src/mob/fight/enum/damageType.ts
+++ src/mob/fight/enum/damageType.ts
@@ -7,6 +7,7 @@ export enum DamageType {
   Fire,
   Frost,
   Electric,
+  Energy,
   Poison,
   Acid,
 }
