70bc8ad314a28278b26be3c815dbf9f47a08833f
Dan Munro <dan@danmunro.com>
2019-06-16 21:58:11 -0500

    max number of retries

--- bin/start-forever.sh
+++ bin/start-forever.sh
@@ -1,1 +1,1 @@
-forever start -c "node_modules/.bin/ts-node --max_old_space_size=4096" -m 3 --spinSleepTime 10000 --minUptime 20000 ./forever.json 3001 5151
+forever start -c "node_modules/.bin/ts-node --max_old_space_size=4096" -m 10 --spinSleepTime 10000 --minUptime 20000 ./forever.json 3001 5151
