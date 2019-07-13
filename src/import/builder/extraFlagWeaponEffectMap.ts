e6f2344ae2e846705b5df056cf6689fd87fab93c
Dan Munro <dan@danmunro.com>
2019-07-13 12:16:17 -0500

    reduce complexity

---
+++ src/import/builder/extraFlagWeaponEffectMap.ts
@@ -0,0 +1,26 @@
+import {WeaponEffect} from "../../item/enum/weaponEffect"
+import Maybe from "../../support/functional/maybe"
+
+export interface ExtraFlagWeaponEffectMapType {
+  [key: string]: WeaponEffect,
+}
+
+const map: ExtraFlagWeaponEffectMapType = {
+  A: WeaponEffect.Flaming,
+  B: WeaponEffect.Frost,
+  C: WeaponEffect.Vampiric,
+  D: WeaponEffect.Sharp,
+  E: WeaponEffect.Vorpal,
+  G: WeaponEffect.Shocking,
+  H: WeaponEffect.Poison,
+  I: WeaponEffect.Stun,
+  J: WeaponEffect.Holy,
+  K: WeaponEffect.Favored,
+  L: WeaponEffect.Nether,
+  M: WeaponEffect.Scion,
+  N: WeaponEffect.ResistEnchant,
+}
+
+export function maybeExtraFlagWeaponWeaponEffect(flag: string) {
+  return new Maybe(map[flag])
+}
--- src/import/builder/weapon.ts
+++ src/import/builder/weapon.ts
@@ -1,53 +1,22 @@
 import {ItemEntity} from "../../item/entity/itemEntity"
-import {WeaponEffect} from "../../item/enum/weaponEffect"
 import {WeaponType} from "../../item/enum/weaponType"
 import {newWeapon} from "../../item/factory/itemFactory"
 import {DamageType} from "../../mob/fight/enum/damageType"
+import Maybe from "../../support/functional/maybe"
+import {maybeExtraFlagWeaponWeaponEffect} from "./extraFlagWeaponEffectMap"
 import ItemPrototype from "./itemPrototype"
 
 export default function(itemPrototype: ItemPrototype): ItemEntity {
   const { name, description, extraFlag, args } = itemPrototype
   const weapon = newWeapon(name, description, args[0] as WeaponType, args[3] as DamageType)
-  if (extraFlag) {
-    extraFlag.split("").forEach((flag: string) => {
-      const effect = getWeaponEffectFromExtraFlag(flag)
-      if (effect) {
-        weapon.weaponEffects.push(effect)
-      }
+  return new Maybe(extraFlag)
+    .do(extra => {
+      extra.split("").forEach((flag: string) =>
+        maybeExtraFlagWeaponWeaponEffect(flag)
+          .do(weaponEffect => weapon.weaponEffects.push(weaponEffect))
+          .get())
+      return weapon
     })
-  }
-  return weapon
-}
-
-function getWeaponEffectFromExtraFlag(flag: string): WeaponEffect | undefined {
-  switch (flag) {
-    case "A":
-      return WeaponEffect.Flaming
-    case "B":
-      return WeaponEffect.Frost
-    case "C":
-      return WeaponEffect.Vampiric
-    case "D":
-      return WeaponEffect.Sharp
-    case "E":
-      return WeaponEffect.Vorpal
-    case "G":
-      return WeaponEffect.Shocking
-    case "H":
-      return WeaponEffect.Poison
-    case "I":
-      return WeaponEffect.Stun
-    case "J":
-      return WeaponEffect.Holy
-    case "K":
-      return WeaponEffect.Favored
-    case "L":
-      return WeaponEffect.Nether
-    case "M":
-      return WeaponEffect.Scion
-    case "N":
-      return WeaponEffect.ResistEnchant
-    default:
-      return
-  }
+    .or(() => weapon)
+    .get()
 }
