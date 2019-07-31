c2b07d037abede024273c3f52691cc0cd265c75f
Dan Munro <dan@danmunro.com>
2019-07-31 16:29:10 -0700

    reduce complexity

--- src/import/service/mob/mobTraits.ts
+++ src/import/service/mob/mobTraits.ts
@@ -1,84 +1,48 @@
 import {MobEntity} from "../../../mob/entity/mobEntity"
 import {MobTrait} from "../../enum/mobTrait"
+import Maybe from "../../../support/functional/maybe/maybe"
+
+type MobTraitTransformer = (mob: MobEntity) => void
+
+interface MobTraitMapper {
+  mobTrait: MobTrait,
+  transform: MobTraitTransformer
+}
+
+function createTransformer(mobTrait: MobTrait, transform: MobTraitTransformer): MobTraitMapper {
+  return { mobTrait, transform }
+}
+
+const mobTraitTransformerMap: MobTraitMapper[] = [
+  createTransformer(MobTrait.IsNpc, mob => mob.traits.isNpc = true),
+  createTransformer(MobTrait.Sentinel, mob => mob.traits.wanders = true),
+  createTransformer(MobTrait.Scavenger, mob => mob.traits.scavenger = true),
+  createTransformer(MobTrait.Aggressive, mob => mob.traits.aggressive = true),
+  createTransformer(MobTrait.StayArea, mob => mob.traits.stayArea = true),
+  createTransformer(MobTrait.Wimpy, mob => mob.traits.wimpy = true),
+  createTransformer(MobTrait.Pet, mob => mob.traits.isPet = true),
+  createTransformer(MobTrait.Trainer, mob => mob.traits.trainer = true),
+  createTransformer(MobTrait.Practice, mob => mob.traits.practice = true),
+  createTransformer(MobTrait.Undead, mob => mob.traits.undead = true),
+  createTransformer(MobTrait.Weaponsmith, mob => mob.traits.weaponsmith = true),
+  createTransformer(MobTrait.Armorer, mob => mob.traits.armorer = true),
+  createTransformer(MobTrait.Cleric, mob => mob.traits.cleric = true),
+  createTransformer(MobTrait.Mage, mob => mob.traits.mage = true),
+  createTransformer(MobTrait.Ranger, mob => mob.traits.ranger = true),
+  createTransformer(MobTrait.Warrior, mob => mob.traits.warrior = true),
+  createTransformer(MobTrait.NoAlign, mob => mob.traits.noAlign = true),
+  createTransformer(MobTrait.NoPurge, mob => mob.traits.noPurge = true),
+  createTransformer(MobTrait.Outdoors, mob => mob.traits.outdoors = true),
+  createTransformer(MobTrait.Indoors, mob => mob.traits.indoors = true),
+  createTransformer(MobTrait.Mount, mob => mob.traits.mount = true),
+  createTransformer(MobTrait.Healer, mob => mob.traits.healer = true),
+  createTransformer(MobTrait.Gain, mob => mob.traits.gain = true),
+  createTransformer(MobTrait.Changer, mob => mob.traits.changer = true),
+  createTransformer(MobTrait.NoTrans, mob => mob.traits.noTrans = true),
+]
 
 export default function(mob: MobEntity, trait: MobTrait) {
-  switch (trait) {
-    case MobTrait.IsNpc:
-      mob.traits.isNpc = true
-      return
-    case MobTrait.Sentinel:
-      mob.traits.wanders = false
-      return
-    case MobTrait.Scavenger:
-      mob.traits.scavenger = true
-      return
-    case MobTrait.Aggressive:
-      mob.traits.aggressive = true
-      return
-    case MobTrait.StayArea:
-      mob.traits.stayArea = true
-      return
-    case MobTrait.Wimpy:
-      mob.traits.wimpy = true
-      return
-    case MobTrait.Pet:
-      mob.traits.isPet = true
-      return
-    case MobTrait.Trainer:
-      mob.traits.trainer = true
-      return
-    case MobTrait.Practice:
-      mob.traits.practice = true
-      return
-    case MobTrait.Undead:
-      mob.traits.undead = true
-      return
-    case MobTrait.Weaponsmith:
-      mob.traits.weaponsmith = true
-      return
-    case MobTrait.Armorer:
-      mob.traits.armorer = true
-      return
-    case MobTrait.Cleric:
-      mob.traits.cleric = true
-      return
-    case MobTrait.Mage:
-      mob.traits.mage = true
-      return
-    case MobTrait.Ranger:
-      mob.traits.ranger = true
-      return
-    case MobTrait.Warrior:
-      mob.traits.warrior = true
-      return
-    case MobTrait.NoAlign:
-      mob.traits.noAlign = true
-      return
-    case MobTrait.NoPurge:
-      mob.traits.noPurge = true
-      return
-    case MobTrait.Outdoors:
-      mob.traits.outdoors = true
-      return
-    case MobTrait.Indoors:
-      mob.traits.indoors = true
-      return
-    case MobTrait.Mount:
-      mob.traits.mount = true
-      return
-    case MobTrait.Healer:
-      mob.traits.healer = true
-      return
-    case MobTrait.Gain:
-      mob.traits.gain = true
-      return
-    case MobTrait.Changer:
-      mob.traits.changer = true
-      return
-    case MobTrait.NoTrans:
-      mob.traits.noTrans = true
-      return
-    default:
-      console.error("unknown trait", trait)
-  }
+  Maybe.if(
+    mobTraitTransformerMap.find(t => t.mobTrait === trait),
+    transformer => transformer.transform(mob))
 }
