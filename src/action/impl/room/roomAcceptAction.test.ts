e4dc9cb9f80a6fd59380161ee4b59a63d6987507
Dan Munro <dan@danmunro.com>
2019-08-02 09:46:51 -0700

    tests for room accept action

--- src/action/impl/room/roomAcceptAction.test.ts
+++ src/action/impl/room/roomAcceptAction.test.ts
@@ -1,4 +1,5 @@
 import {createTestAppContainer} from "../../../app/factory/testFactory"
+import {MobEntity} from "../../../mob/entity/mobEntity"
 import {RequestType} from "../../../request/enum/requestType"
 import {RealEstateListingEntity} from "../../../room/entity/realEstateListingEntity"
 import RealEstateService from "../../../room/service/realEstateService"
@@ -7,28 +8,28 @@ import {Types} from "../../../support/types"
 
 let testRunner: TestRunner
 let realEstateService: RealEstateService
+let owner: MobEntity
+let bidder: MobEntity
 
 beforeEach(async () => {
   const app = await createTestAppContainer()
   testRunner = app.get<TestRunner>(Types.TestRunner)
   realEstateService = app.get<RealEstateService>(Types.RealEstateListingService)
+  bidder = (await testRunner.createMob()).get()
+  const room = testRunner.getStartRoom().get()
+  owner = (await testRunner.createMob()).get()
+  room.isOwnable = true
+  room.owner = owner
+  bidder.gold = 10
+  const listing = new RealEstateListingEntity()
+  listing.agent = owner
+  listing.room = room
+  listing.offeringPrice = 10
+  await realEstateService.createListing(listing)
 })
 
 describe("room accept action", () => {
   it("accepts a bid and resolves the transaction", async () => {
-    // setup
-    const bidder = (await testRunner.createMob()).get()
-    const room = testRunner.getStartRoom().get()
-    const owner = (await testRunner.createMob()).get()
-    room.isOwnable = true
-    room.owner = owner
-    bidder.gold = 10
-    const listing = new RealEstateListingEntity()
-    listing.agent = owner
-    listing.room = room
-    listing.offeringPrice = 10
-    await realEstateService.createListing(listing)
-
     // given
     await testRunner.invokeAction(RequestType.RoomBid, "room bid 10")
 
@@ -38,5 +39,39 @@ describe("room accept action", () => {
     // then
     expect(response.getMessageToRequestCreator())
       .toBe(`You accept the bid from ${bidder.name} on Test room 1. You receive 10 gold.`)
+  })
+
+  it("cannot accept a bid for a room that is not owned by the requester", async () => {
+    // setup
+    const accepter = (await testRunner.createMob()).get()
+    await testRunner.invokeAction(RequestType.RoomBid, "room bid 10")
+
+    // when
+    const response = await testRunner.invokeActionAs(accepter, RequestType.RoomAccept, `room accept '${bidder.name}'`)
+
+    // then
+    expect(response.getMessageToRequestCreator())
+      .toBe("You don't own this room.")
+  })
+
+  it("must have a valid matching bid", async () => {
+    // when
+    const response = await testRunner.invokeActionAs(owner, RequestType.RoomAccept, `room accept '${bidder.name}'`)
+
+    // then
+    expect(response.getMessageToRequestCreator())
+      .toBe("That bid was not found.")
+  })
+
+  it("cannot be in a fight", async () => {
+    // setup
+    await testRunner.fight(owner)
+
+    // when
+    const response = await testRunner.invokeActionAs(owner, RequestType.RoomAccept, `room accept '${bidder.name}'`)
+
+    // then
+    expect(response.getMessageToRequestCreator())
+      .toBe("No way! You are fighting.")
   })
 })
