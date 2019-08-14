e567f0e918cb87843c48570b627c3bb08c545b88
Dan Munro <dan@danmunro.com>
2019-08-14 07:11:14 -0500

    cleaning house

--- src/mob/skill/skillTable.ts
+++
@@ -1,7 +0,0 @@
-import {Container} from "inversify"
-import Skill from "../../action/impl/skill"
-import {Types} from "../../support/types"
-
-export function getSkillTable(container: Container): Skill[] {
-  return container.getAll<Skill>(Types.Skills)
-}
