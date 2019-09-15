872ea955b134c6c07b7cc935a3b41932ee26c99f
Dan Munro <dan@danmunro.com>
2019-09-15 08:55:14 -0500

    get running in aws

--- bin/start-forever.sh
+++ bin/start-forever.sh
@@ -1,1 +1,1 @@
-forever start -c "node_modules/.bin/ts-node --max_old_space_size=4096" -m 10 --spinSleepTime 10000 --minUptime 20000 ./forever.json 3001 5151
+NODE_OPTIONS="--max-old-space-size=8192" forever start -c "node_modules/.bin/ts-node" -m 10 --spinSleepTime 10000 --minUptime 20000 ./forever.json 3001 5151
