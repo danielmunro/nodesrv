b2b3d58644affdc18b217b1df005e98d7678250a
Dan Munro <dan@danmunro.com>
2019-09-20 06:38:42 -0500

    add corpse to item service

--- src/mob/eventConsumer/addCorpseToRoomOnDeathEventConsumer.ts
+++ src/mob/eventConsumer/addCorpseToRoomOnDeathEventConsumer.ts
@@ -2,13 +2,16 @@ import {inject, injectable} from "inversify"
 import {EventType} from "../../event/enum/eventType"
 import EventConsumer from "../../event/interface/eventConsumer"
 import EventResponse from "../../event/messageExchange/eventResponse"
+import ItemService from "../../item/service/itemService"
 import DeathEvent from "../../mob/event/deathEvent"
 import {Types} from "../../support/types"
 import LocationService from "../service/locationService"
 
 @injectable()
 export default class AddCorpseToRoomOnDeathEventConsumer implements EventConsumer {
-  constructor(@inject(Types.LocationService) private readonly locationService: LocationService) {}
+  constructor(
+    @inject(Types.LocationService) private readonly locationService: LocationService,
+    @inject(Types.ItemService) private readonly itemService: ItemService) {}
 
   public getConsumingEventTypes(): EventType[] {
     return [ EventType.MobDeath ]
@@ -17,6 +20,7 @@ export default class AddCorpseToRoomOnDeathEventConsumer implements EventConsume
   public async consume(event: DeathEvent): Promise<EventResponse> {
     const room = this.locationService.getRoomForMob(event.death.mobKilled)
     room.inventory.addItem(event.death.corpse)
+    this.itemService.add(event.death.corpse)
     return EventResponse.none(event)
   }
 }
